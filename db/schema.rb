97c1136645b197d3bbf320a9cdee97f1018d988a
Ajmal Khan <ajikhan512@gmail.com>
2024-10-24 17:58:14 +0500

    authorization and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_22_221000) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_22_222505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,8 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_22_221000) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "wallet_address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["wallet_address"], name: "index_users_on_wallet_address", unique: true
   end
 
 end
